2e71c1954591faee48fb5833eb4bd3a81c993553
khajan_bhatt <tanujbhatt8279@gmail.com>
2024-08-31 13:57:25 +0530

    Modifications

--- 1D Array Applications/Missing_Duplicate_Number.cpp
+++ 1D Array Applications/Missing_Duplicate_Number.cpp
@@ -12,8 +12,21 @@ int missingNumber(vector<int>& nums) {
      return asum - fsum;
 }
 
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+//XOR Implementation -> Time : O(n), Space : O(1)
+int singleNumber(vector<int>& nums) {
+     long long alone {nums[0]};
+     unsigned long size {nums.size()};
+     for (unsigned long i = 1; i < size; i++)
+          alone ^= nums[i];
+     return alone;
+}
+
 int main(){
      vector <int> nums {0, 2, 3, 4, 7, 1, 5};
      cout<<missingNumber(nums);
+     nums = {4,1,2,1,2};
+     cout<<singleNumber(nums);
      return 0;
 }
--- 1D Array Baics/Subarrays.cpp
+++ 1D Array Baics/Subarrays.cpp
@@ -2,15 +2,17 @@
 #include <array>
 using namespace std;
 
+//Subarrays are continuous portions of an array that are taken indeoendently. These are different from subsets or subsequences as these contains contiguous elements of an array.
+
 //Subarrays -> Time : O(n), Space : O(1)
 void subarrays (array <int, 3> arr){
      int size = arr.size();
      cout<<"\nSubarrays : "; 
      //The total number of subarrays is size * (size + 1) / 2
-     for (int i = 0; i < size; i++){
-          for (int j = i; j < size; j++){
+     for (int i = 0; i < size; i++){ //For defining start of subarray
+          for (int j = i; j < size; j++){ //For defining end of the subarray
                cout<<"\n[";
-               for (int k = i; k <= j; k++)
+               for (int k = i; k <= j; k++) //For iterating through i->j of current subarray defined
                     cout<<arr[k]<<", ";
                cout<<"\b\b]";
           }
